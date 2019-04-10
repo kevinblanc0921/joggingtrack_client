d4afae2158bdb651e5592f4117a7b5c205963488
kevinblanc0921 <kevinblanc@yandex.com>
2019-04-09 18:41:46 -0700

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,33 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { SignupComponent } from './signup';
+import { RecordListComponent } from './recordList';
+import { AuthGuard } from './_guards';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'recordList',
+        component: RecordListComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
